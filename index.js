042b9235fce8230254c5e812cc51355cb8297747
aday-allbase <maxroepz48@gmail.com>
2024-11-07 02:52:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'e49d7106-c229-47e6-abe9-7189bd33e02f';
 
-const proxyIPs = ['35.219.15.90', '35.219.50.99', '103.133.223.50:2096'];
+const proxyIPs = ['35.219.15.90:443', '35.219.50.99:443', '103.133.223.50:2096'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
